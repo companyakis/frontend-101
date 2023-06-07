54f8d02e60307a65b6f0fc5871c3ae6a9e2d0b9d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-07 19:38:40 +0300

    Create 02_create_complex_jsx.js

---
+++ 02_create_complex_jsx.js
@@ -0,0 +1,12 @@
+const JSX = (
+  <div>
+    <h1>A complex JSX element</h1>
+    <p>Türkçe de olsun</p>
+    <ul>
+      <li>React</li>
+      <li>React Native</li>
+      <li>HTML ve CSS</li>
+      <li>Figma</li>
+    </ul>
+  </div>
+);
