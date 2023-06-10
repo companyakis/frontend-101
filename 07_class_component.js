aee26b43723705fb09a12dcd9a5e5f3874982ea8
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-10 13:18:27 +0300

    Create 07_class_component.js

---
+++ 07_class_component.js
@@ -0,0 +1,14 @@
+class AComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>
+          Hello a Class Component!
+        </h1>
+      </div>
+    );
+  }
+};
